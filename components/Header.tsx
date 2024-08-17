6f2c396bf90c95e9a7c1bedac67cef8fbb654351
sayem-saif <139125238+sayem-saif@users.noreply.github.com>
2024-08-17 07:57:59 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,31 @@
-import Image from 'next/image'
-import React from 'react'
-import logo from '../public/logo.svg'
-import Link from 'next/link'
+import Image from 'next/image';
+import React from 'react';
+import logo from '../public/logo.svg';
+import Link from 'next/link';
 
-function Header() {
-  return (
-    <header className='mb-16 flex h-16 w-screen item-center justify-center pt-16'>
-      <div className='item-center flex justify-center md:ml-10 md:w1/5'> 
-        <Link href="/">
-        <div className='relative opacity-75 transition duration-300 hover:opacity-100'>
-        <div className='flex item-center'>
-        <Image src={logo} alt='logo' className='w-12'/>
-        <h1 className='ml-2 font-bold text-lg'>ShiFlix.</h1>
-        </div>
-        </div>
-        </Link>
-    </div>
-
-        <nav className='hidden flex-1 items-center justify-center space-x-10 md:ml-16 md:flex'>
-        <p>Made with <span role='img' aria-label='heart emoji'>❤️</span> by <a target='_blank' className='text-blue-500 hover:underline' href='https://github.com/sayem-saif'>Devloper Saif</a></p>
-    </nav>
-    </header>
-    
-  )
+interface HeaderProps {
+  className?: string;  // Allow className prop
 }
 
-export default Header
+const Header: React.FC<HeaderProps> = ({ className }) => {
+  return (
+    <header className={`mb-16 flex h-16 w-screen items-center justify-center pt-16 ${className}`}>
+      <div className='items-center flex justify-center md:ml-10 md:w-1/5'> 
+        <Link href="/">
+          <div className='relative opacity-75 transition duration-300 hover:opacity-100'>
+            <div className='flex items-center'>
+              <Image src={logo} alt='logo' className='w-12'/>
+              <h1 className='ml-2 font-bold text-lg'>ShiFlix.</h1>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <nav className='hidden flex-1 items-center justify-center space-x-10 md:ml-16 md:flex'>
+        <p>Made with <span role='img' aria-label='heart emoji'>❤️</span> by <a target='_blank' className='text-blue-500 hover:underline' href='https://github.com/sayem-saif'>Developer Saif</a></p>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
